e5d5047b65d0ed989ad40b9d6c0670aebade5f90
Damián Grimaldi <grimaldidamain001@gmail.com>
2025-04-06 20:25:43 -0300

    Deteccion del encabezado y de las preguntas

--- problema2.py
+++ problema2.py
@@ -4,32 +4,44 @@ import matplotlib.pyplot as plt
 
 def deteccion_renglones(img):
     """
-    Detecta los renglones en una imagen utilizando la transformada de Hough.
+    Detecta los renglones en una imagen.
     Args:
         img (numpy.ndarray): Imagen de entrada en escala de grises.
     Returns:
         numpy.ndarray: Imagen con los renglones detectados.
     """
     img_copy = img.copy()
-    img_copy[img_copy <= 110] = 0
-    img_copy[img_copy > 110] = 255
+    img_copy[img_copy <= 130] = 0
+    img_copy[img_copy > 130] = 255
 
     img_zeros = img_copy == 0
 
     img_row_zeros = img_zeros.any(axis=1)
 
     x = np.diff(img_row_zeros)          
-    renglones_indxs = np.argwhere(x)
-    renglones_indxs = renglones_indxs[[0, 3]]
-    len(renglones_indxs)
+    renglones_indxs = np.argwhere(x).flatten()
 
-    fila_inicio = renglones_indxs[0, 0] + 1
-    fila_fin = renglones_indxs[1, 0] + 1
+    fila_inicio_encabezado = renglones_indxs[2] + 1
+    fila_fin_encabezado = renglones_indxs[3] + 1
+    encabezado = img_copy[fila_inicio_encabezado:fila_fin_encabezado, :]
 
-    renglon_img = img_copy[fila_inicio:fila_fin, :]
+    preguntas = []
+    for i in range(4, len(renglones_indxs)-1, 2):  # Avanzamos de 2 en 2 para pares (inicio, fin)
+        y1 = renglones_indxs[i] + 1
+        y2 = renglones_indxs[i + 1] + 1
 
-    return renglon_img
+        if y2 - y1 > 10:  # Aseguramos que sea un bloque con contenido
+            pregunta = img_copy[y1:y2, :]
+            preguntas.append(pregunta)
 
-img = cv2.imread('TP 1/examen_1.png', cv2.IMREAD_GRAYSCALE)
+    return encabezado, preguntas
 
-renglon = deteccion_renglones(img)
+img = cv2.imread('TP 1/multiple_choice_1.png', cv2.IMREAD_GRAYSCALE)
+
+respuestas_correctas = [
+    "A", "A", "B", "A", "D", "B", "B", "C", "B", "A",
+    "D", "A", "C", "C", "D", "B", "A", "C", "C", "D",
+    "B", "A", "C", "C", "C"
+]
+
+encabezado, preguntas = deteccion_renglones(img)
